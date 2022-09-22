87c01620df18af45c33d8f13777ad5ad9114d5f5
GryphonAblaze <112128392+GryphonAblaze@users.noreply.github.com>
2022-09-22 16:12:26 -0400

    did it!

--- UI/End_Game.gd
+++ UI/End_Game.gd
@@ -7,7 +7,7 @@ func _ready():
 
 func _on_Play_again_pressed():
 	Global.reset()
-	var _scene = get_tree().chagne_scene("res://Game.tscn")
+	var _scene = get_tree().change_scene("res://Game.tscn")
 
 
 func _on_Quit_pressed():
